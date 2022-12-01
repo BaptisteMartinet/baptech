021c36d6554481157a5398f5ae7781050db5d09d
Baptiste Martinet <baptiste.martinet@epitech.eu>
2022-12-01 19:57:55 +0100

    feat(components/pages): add Hero text

--- src/components/pages/Home/Overlay/Hero.tsx
+++ src/components/pages/Home/Overlay/Hero.tsx
@@ -5,12 +5,62 @@ const Container = styled('div')({
   width: '100%',
 });
 
+const Title = styled('h1')({
+  marginBlock: '.5em',
+  fontSize: 'var(--fs-l)',
+  color: 'black',
+  '& span': {
+    color: 'rgba(0, 0, 0, 0.4)',
+  },
+});
+
+const SubTitle = styled('h2')({
+  marginBlock: '.5em',
+  fontSize: 'var(--fs-xl)',
+  '& span': {
+    fontSize: 'var(--fs-l)',
+    fontWeight: 'normal',
+  },
+});
+
+const Info = styled('p')({
+  fontSize: 'var(--fs-m)',
+  lineHeight: 'var(--fs-l)',
+  '& span, a': {
+    textDecoration: 'underline',
+  },
+});
+
 function Hero() {
   return (
     <Container>
-      <h1>Baptiste Martinet</h1>
-      <h2>Full Stack developer</h2>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga mollitia, dolore cum, veritatis nam recusandae fugit architecto ab beatae cupiditate, sequi ipsum facere ducimus cumque debitis nesciunt quis assumenda dolores.</p>
+      <Title>
+        baptiste
+        <span>martinet</span>
+      </Title>
+      <SubTitle>
+        Full Stack
+        <span> developer</span>
+      </SubTitle>
+      <Info>
+        Freelance web developer based in
+        {' '}
+        <span>Paris, France</span>
+        🥐.
+        <br />
+        I&apos;m fluent in both French and English.
+        <br />
+        I build backends with
+        {' '}
+        <a href="https://graphql.org/">GraphQL</a>
+        , web apps with
+        {' '}
+        <a href="https://reactjs.org/">React</a>
+        , and mobile apps with
+        {' '}
+        <a href="https://reactnative.dev/">React-native</a>
+        .
+      </Info>
     </Container>
   );
 }
